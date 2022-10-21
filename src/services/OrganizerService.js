b7fbc07f590fd9618a75cef8d6057ee88707c70f
atomfaster@gmail.com <atomfaster@gmail.com>
2022-10-21 11:00:29 +0700

    fix

--- src/services/OrganizerService.js
+++ src/services/OrganizerService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const apiClient = axios.create({
-  baseURL: process.env.VUE_APP_BACKEND_URL,
+  baseURL: "http://3.86.215.64:8999",
   withCredentials: false,
   headers: {
     Accept: 'application/json',
